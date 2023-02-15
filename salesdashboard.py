3a743f1aa89975a69ff8ec73039fcdf42e4a3835
waqas275 <124732870+waqas275@users.noreply.github.com>
2023-02-16 04:35:07 +0500

    Update salesdashboard.py

--- salesdashboard.py
+++ salesdashboard.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+st.set_page_config(page_title="Sales Dashboard Example", page_icon=":bar_chart:", layout="wide")
 
 
 # In[3]:
